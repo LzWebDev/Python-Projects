eb910bd7a7e1a62723ccc0040d12d16b80cb13da
Luiz Web Developer <meloluizdev20@gmail.com>
2025-04-12 17:29:20 -0300

    feita sabomba, era pra não repetir as perguntas, mas essa merda ainda está repetindo

--- src/quiz.py
+++ src/quiz.py
@@ -1,8 +1,31 @@
 import array
 import json
+import random
 
-# Abre o arquivo
-with open('src/perguntas.json', 'r', encoding='utf-8') as alternativa:
-    perguntas = json.load(alternativa)
+def ler_perguntas():
+    try:
+        with open('src/perguntas.json', 'r', encoding='utf-8' ) as arquivo:
+            return list(json.load(arquivo).values())
+    except FileNotFoundError:
+        return []  # Caso o arquivo não exista, retorna uma lista vazia
 
-print(perguntas['alternativa-1'])
+perguntas_feitas = []
+
+def escolher_pergunta(perguntas, feitas):
+    perguntas_restantes = [p for p in perguntas if p not in feitas]
+    
+    # Se a lista de perguntas estiver vazia, retornar None
+    if not perguntas_restantes:
+        print("Todas as perguntas já foram feitas.")
+        return None
+
+    nova = random.choice(perguntas_restantes)
+    feitas.append(nova)
+    return nova
+
+perguntas = ler_perguntas()
+
+for _ in range(1):
+    pergunta = escolher_pergunta(perguntas, perguntas_feitas)
+    if pergunta:
+        print(f"Pergunta: {pergunta}")
